92aa21baec6f83dbd5280849b8456519608c5044
loihnt05 <23520869@gm.uit.edu.vn>
2025-07-18 14:53:52 +0700

    flash card using carousel of antd

--- flash-card/src/App.tsx
+++ flash-card/src/App.tsx
@@ -1,17 +1,50 @@
-import { Carousel, Progress} from "antd";
-
+import { Carousel, Progress } from "antd";
+import data from "./seed-data";
+import { useState } from "react";
+const contentStyle =
+  "h-[300px] flex items-center justify-center text-white text-center text-2xl";
 function App() {
+  const [shownIndex, setShownIndex] = useState<number | null>(null);
+  const [currentPercent, setCurrentPercent] = useState(1/data.length * 100); 
   return (
-    <div className="flex flex-col justify-center items-center w-[70%]">
-      <p className="font-bold text-2xl font-sans ">Flash Card</p>
-      <div className="App">
+    <div className="flex flex-col justify-center items-center p-10">
+      <div>
+        <p className="font-bold text-4xl font-sans ">Flash Card</p>
       </div>
-          <Progress percent={100} />
-              <Carousel arrows infinite={false} className="bg-amber-800">
-                <div>hello</div>
-                <div>there</div>
-                <div>world</div>
-              </Carousel>
+      <div className="w-1/2 my-2">
+        <Progress
+          percent={currentPercent}
+          percentPosition={{ align: "end", type: "inner" }}
+          size={[585, 30]}
+          strokeColor="#0f4868"
+        />
+      </div>
+      <div className="w-1/2">
+        <Carousel
+        afterChange={(current) => {
+            setCurrentPercent(Math.round(((current + 1) / data.length) * 100));
+          }}
+          slickGoTo={2}
+          arrows
+          infinite={false}
+          className="bg-[#364d79] text-white font-semibold border-gray-400 border-solid border-5 h-auto min-h-75 p-5 rounded-xl"
+        >
+          {data.map((item, index) => (
+            <div
+              key={index}
+              onClick={() => {
+                if (shownIndex === index) setShownIndex(-1);
+                else setShownIndex(index);
+              }}
+              className="cursor-pointer"
+            >
+              <div className={contentStyle}>
+                {shownIndex === index ? item.answer : item.question}
+              </div>
+            </div>
+          ))}
+        </Carousel>
+      </div>
     </div>
   );
 }
---
+++ flash-card/src/seed-data.ts
@@ -0,0 +1,28 @@
+type FlashCard = {
+  question: string;
+  answer: string;
+};
+const data: FlashCard[] = [
+    {
+        question: "What is the capital of France?",
+        answer: "Paris"
+    },
+    {
+        question: "What is the largest planet in our solar system?",
+        answer: "Jupiter"
+    },
+    {
+        question: "What is the chemical symbol for gold?",
+        answer: "Au"
+    },
+    {
+        question: "Who wrote 'To Kill a Mockingbird'?",
+        answer: "Harper Lee"
+    },
+    {
+        question: "What is the powerhouse of the cell?",
+        answer: "Mitochondria"
+    },
+]
+
+export default data;
